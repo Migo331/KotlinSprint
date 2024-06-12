67c7db053b23c37e56168775ce8f231b5d3cc3b2
Alex Volkov <marsplanet4@yandex.ru>
2024-06-12 17:23:54 +0200

    KS-1-1 task-1-2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75;
+    val gratitudeText: String = "Благодарим за покупку!"
+
+    println(numberOfOrders)
+    println(gratitudeText)
+
+    var workers: Int = 2000
+
+// println(workers)
+
+    workers = 1999
+
+    println(workers)
+}
