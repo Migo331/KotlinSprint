28f28941002eee00028d843bd5c7ce1ca241417d
Alex Volkov <marsplanet4@yandex.ru>
2024-06-13 21:16:12 +0200

    KS-1-5 доработка

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -5,16 +5,17 @@ import java.time.LocalTime
 
 
 fun main() {
-
-
     val seconds = 6480
-    val minutes = seconds/60
-    val hours = minutes/60
-     println(LocalTime.of(minutes,hours,seconds))
-
-
-
-
+    val minute = seconds % 60
+    println(minute)
+    val minutes = seconds / 60
+    println(minutes)
+    val hour = minutes / 60
+    println(hour)
+    val hours = minutes % 60
+    println(hours)
+    val time = "${hour}:${hours}:${minute}"
+    println(time)
 
 }
 
