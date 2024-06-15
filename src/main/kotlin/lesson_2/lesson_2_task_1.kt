6ccb1fe3ccf113e2c877511220bc49a2a006c860
Alex Volkov <marsplanet4@yandex.ru>
2024-06-15 10:22:59 +0200

    KS-2-1 - доработка

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -2,6 +2,12 @@ package org.example.lesson_2
 
 fun main() {
 
-    val middleMark = (3+4+3+5)/4f
+    val persons = 4
+    val bal1 = 3
+    val bal2 = 4
+    val bal3 = 3
+    val bal4 = 5
+
+    val middleMark = (3 + 4 + 3 + 5) / 4.toFloat()
     println(middleMark)
 }
